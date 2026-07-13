a2b8ccb29a2e481355389c3e29caf53a4eadef1f
Abhinav Singhal <abhinavsinghal283@gmail.com>
2026-07-13 14:48:44 +0530

    Time: 0 ms (100%), Space: 46.7 MB (52.22%) - LeetHub

---
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -0,0 +1,37 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        if(head == null) return false;
+
+        // TortoiseHair Algorithm
+        ListNode slow=head;
+        ListNode fast = head;
+        while(fast != null && fast.next != null){
+            slow=slow.next;
+            fast=fast.next.next;
+            if(slow == fast) return true;
+        }
+        return false;
+
+
+        // HashSet<ListNode> hs = new HashSet<>();
+        // ListNode temp =head;
+        // while(temp.next!=null){
+        //     hs.add(temp);
+        //     temp=temp.next;
+        //     if(hs.contains(temp)) return true;
+        // }
+        // return false;
+
+    }
+}
